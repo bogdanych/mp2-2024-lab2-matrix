dc5e78aeb1d57e6d579d43e6af189c13d3712325
AlemaevMaxim <47293022+bogdanych@users.noreply.github.com>
2024-11-16 23:42:01 +0300

    sample_matrix.cpp

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -1,4 +1,4 @@
-﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
 //
 // Copyright (c) Сысоев А.В.
 //
@@ -10,21 +10,55 @@
 
 void main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите размеры матриц\n";
+	int size;
+	cin >> size;
+	cout << "Введите матрицу A\n";
+	TDynamicMatrix<int> a(size);
+	cin >> a;
+	cout << '\n';
+	cout << "Введите матрицу B\n";
+	TDynamicMatrix<int> b(size);
+	cin >> b;
+	cout << '\n';
+	string s = "0";
+	do {
+		cout << "e - выйти, m - перемножить A и B, p - сложить A и B, d - вычесть из A B, ta - умножить A на скаляр, tb - умножить B на скаляр va - умножить A на вектор, vb - умножить B на вектор\n";
+		cin >> s;
+		if (s == "m")
+			cout << a * b << '\n';
+		else if (s == "p")
+			cout << a + b << '\n';
+		else if (s == "d")
+			cout << a - b << '\n';
+		else if (s == "ta") {
+			int t;
+			cout << "значение скаляра: \n";
+			cin >> t;
+			cout << a * t << '\n';
+		}
+		else if (s == "tb") {
+			int t;
+			cout << "значение скаляра: \n";
+			cin >> t;
+			cout << b * t << '\n';
+		}
+		else if (s == "va") {
+			cout << "Введите элементы вектора:\n";
+			TDynamicVector<int> v(size);
+			cin >> v;
+			cout << a * v << '\n';
+		}
+		else if (s == "vb") {
+			cout << "Введите элементы вектора:\n";
+			TDynamicVector<int> v(size);
+			cin >> v;
+			cout << b * v << '\n';
+		}
+		else if (s != "e")
+			cout << "Некоректный ввод\n";
+	} while (s != "e");
 }
+
 //---------------------------------------------------------------------------
